efd2f59d8d0a1dc2c735909dde940082ab9b9c13
Andrew M Krug <andrew.m.krug@gmail.com>
2023-01-22 18:15:56 -0500

    fix(ts): resolutions

--- applications/site/astro.config.mjs
+++ applications/site/astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-export default defineConfig({});
+import image from "@astrojs/image"
+import mdx from "@astrojs/mdx"
+import node from "@astrojs/node"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image(), mdx()],
+  output: "server",
+  adapter: node({ mode: "middleware" }),
+})
