d74b3c9677685a099ce0ed4556ae514bea737955
Andrew M Krug <andrew.m.krug@gmail.com>
2023-01-26 22:58:04 -0500

    fix(deps): latest

---
+++ tools/repo-docs/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config"
+
+// https://astro.build/config
+import image from "@astrojs/image"
+import mdx from "@astrojs/mdx"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image(), mdx()],
+})
